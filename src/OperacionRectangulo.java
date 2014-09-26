cb34c6b8151d2cac50a428509856aebdf9dad2f7
giovanyhernandez <giovany9216@gmail.com>
2014-09-26 08:59:55 +0200

    creado

--- src/OperacionRectangulo.java
+++ src/OperacionRectangulo.java
@@ -1,1 +1,27 @@
-clasclass
+import java.util.Scanner; 
+
+public class OperacionRectangulo{
+
+public static void main (String args []){
+
+	double ladoa, ladob, resulp, resula;
+	Rectangulo Operaciones = new  Rectangulo;
+
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("\ingresa el ladoa: ");
+	ladoa = sc.nextDouble();
+ 
+
+	resula = Rectangulo.area();
+	resulp = Rectangulo.perimetro();
+
+
+  System.out.println("la area es : " + resula);  
+  System.out.println("el perimetro  es : " + resulp);  
+
+
+    }
+}
+
+
---
+++ src/Rectangulo.java
@@ -0,0 +1,21 @@
+public class Rectangulo {
+
+public double perimetro (){
+
+        double result = 0;
+        result = (2*ladoa) + (2*ladob);
+        return result;
+
+
+
+        }
+
+public double area (){
+
+        double result = 0;
+        result = ladoa*ladob;
+        return result;
+
+
+        }
+}  
